676f88588a43c88d7c3e7cc69bc016d98a6a5bce
sk8ingDuck <alexander.ch.atanasov@gmail.com>
2023-03-18 15:17:05 +0100

    libs

--- FriendSystemGUI-Spigot/src/main/java/me/sk8ingduck/friendsystemgui/listener/InventoryClickListener.java
+++ FriendSystemGUI-Spigot/src/main/java/me/sk8ingduck/friendsystemgui/listener/InventoryClickListener.java
@@ -1,13 +1,13 @@
 package me.sk8ingduck.friendsystemgui.listener;
 
 import me.sk8ingduck.friendsystemgui.FriendSystemGUI;
-import me.sk8ingduck.friendsystemgui.config.GuiConfig;
 import me.sk8ingduck.friendsystemgui.config.SettingsConfig;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class InventoryClickListener implements Listener {
 
@@ -16,15 +16,27 @@ public class InventoryClickListener implements Listener {
 		Player player = (Player) event.getWhoClicked();
 
 		SettingsConfig config = FriendSystemGUI.getInstance().getSettingsConfig();
-		GuiConfig guiConfig = FriendSystemGUI.getInstance().getGuiConfig();
-		if (config.isCanMoveItemInInventory()
-				|| player.getGameMode() == GameMode.CREATIVE
-				|| player.getInventory().getItemInHand().getItemMeta() == null
-				|| guiConfig.get("guiItem").getItemMeta() == null
-				|| player.getInventory().getItemInHand().getItemMeta().getDisplayName()
-				.equalsIgnoreCase(guiConfig.get("guiItem").getItemMeta().getDisplayName()))
+		if (config.isCanMoveItemInInventory() || player.getGameMode() == GameMode.CREATIVE) {
 			return;
+		}
 
-		event.setCancelled(true);
+		String playerHead = FriendSystemGUI.getInstance().getGuiConfig().get("guiItem").getItemMeta().getDisplayName();
+
+		if (event.getHotbarButton() >= 0 && event.getHotbarButton() <= player.getInventory().getSize()) {
+			ItemStack item1 = player.getInventory().getItem(event.getHotbarButton());
+
+			if (item1 != null
+					&& item1.getItemMeta() != null
+					&& item1.getItemMeta().getDisplayName().equalsIgnoreCase(playerHead)) {
+				event.setCancelled(true);
+				return;
+			}
+		}
+		ItemStack item2 = event.getCurrentItem();
+		if (item2 != null
+				&& item2.getItemMeta() != null
+				&& item2.getItemMeta().getDisplayName().equalsIgnoreCase(playerHead)) {
+			event.setCancelled(true);
+		}
 	}
 }
